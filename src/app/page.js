8dc1552a1543e2c46302224ea180d794398e4207
coriander-star <1721007829@qq.com>
2025-06-01 16:10:39 +0800

    Add files via upload

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+import Hello from "../components/Hello";
+import HomeworkCard from "../components/HomeworkCard";  
+export default function Home() {
+  return (
+    <div>
+        <h1>这是来自函数的内容</h1>
+        <HomeworkCard/>
+        <HomeworkCard/>
+        <HomeworkCard/>
+    </div>
+  )
+}
